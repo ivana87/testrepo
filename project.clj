75e6faa08dd3c48c4532b7bb7d57415abdeff22b
ivana87 <Kostasp1>
2017-01-09 12:38:47 +0100

    lein-release plugin: preparing 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject testapp "0.1.1-SNAPSHOT"
+(defproject testapp "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
